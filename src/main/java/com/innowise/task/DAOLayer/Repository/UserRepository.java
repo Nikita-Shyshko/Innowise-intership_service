d8db4791106dd556706dcab895f41575be73c7eb
Nikita <mikita.shyshko@gmail.com>
2026-03-04 03:45:37 +0300

    Add repositories

---
+++ src/main/java/com/innowise/task/DAOLayer/Repository/UserRepository.java
@@ -0,0 +1,29 @@
+package com.innowise.task.DAOLayer.Repository;
+
+import com.innowise.task.Entity.Users;
+import com.innowise.task.UserStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users, Long>, JpaSpecificationExecutor<Users>
+{
+    @Query("select r from Users r where r.id = :id")
+    Optional<Users> getUsersById(@Param("id") Long id);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE Users SET name = :name, surname = :surname, email = :email WHERE id = :id")
+    int updateUserById(@Param("id") Long id,
+                       @Param("name") String name,
+                       @Param("surname") String surname,
+                       @Param("email") String email);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE Users r set r.active = :active where r.id = :id")
+    int setActiveStatusOfUsers(@Param("id") Long id,
+                               @Param("active") UserStatus active);
+}
