940e71128e6614f4353517e6a195f30a4edc142e
Nikita <mikita.shyshko@gmail.com>
2026-04-27 18:04:05 +0300

    Plus CI pipeline + fix tests

--- src/main/resources/db.changelog/sql/Auditing.sql
+++ src/main/resources/db.changelog/sql/Auditing.sql
@@ -1,18 +1,18 @@
 CREATE OR REPLACE FUNCTION set_initial_audit()
-RETURNS TRIGGER AS $$
+RETURNS TRIGGER AS '
 BEGIN
     NEW.created_at = CURRENT_TIMESTAMP;
     NEW.updated_at = CURRENT_TIMESTAMP;
     NEW.version = 0;
-RETURN NEW;
+    RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+' LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION update_audit()
-RETURNS TRIGGER AS $$
+RETURNS TRIGGER AS '
 BEGIN
     NEW.updated_at = CURRENT_TIMESTAMP;
     NEW.version = OLD.version + 1;
-RETURN NEW;
+    RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+' LANGUAGE plpgsql;
--- src/test/resources/db.changelog/sql/create-function-update-audit.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION update_audit()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = CURRENT_TIMESTAMP;
-    NEW.version = OLD.version + 1;
-RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
